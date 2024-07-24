74d45669cd1076443b81cc3156dc72b0e09df7c8
Javokhir Abdullaev <javokhir_abdullaev@epam.com>
2024-07-24 10:44:04 +0500

    add streamlit

--- qa-chatbot-langchain/main.py
+++ qa-chatbot-langchain/main.py
@@ -9,10 +9,13 @@ from langchain.prompts.chat import (
 )
 from langchain.schema import StrOutputParser
 
+from langchain_openai import OpenAIEmbeddings
+
+
 from langchain_community.document_loaders import TextLoader
-from langchain_community.embeddings import SentenceTransformerEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.vectorstores import Chroma
+from langchain.schema.runnable import RunnablePassthrough
 
 load_dotenv()
 
@@ -20,39 +23,59 @@ load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 LANGUAGE_MODEL = "gpt-3.5-turbo-instruct"
 
-system_prompt = "You are a helpful assistant that answers generals inquiries and assist with technical issues"
-
-str_parser = StrOutputParser()
-
-# basic example of how to get started with the OpenAI Chat models
-# The above cell assumes that your OpenAI API key is set in your environment variables.
-model = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.3)
-
-system_message_prompt = SystemMessagePromptTemplate.from_template(system_prompt)
-human_message_prompt = HumanMessagePromptTemplate.from_template("{question}")
-chat_prompt = ChatPromptTemplate.from_messages(
+template: str = """/
+    You are a customer support specialist /
+    question: {question}. You assist users with general inquiries based on {context} /
+    and  technical issues. /
+    """
+system_message_prompt = SystemMessagePromptTemplate.from_template(template)
+human_message_prompt = HumanMessagePromptTemplate.from_template(
+    input_variables=["question", "context"],
+    template="{question}",
+)
+chat_prompt_template = ChatPromptTemplate.from_messages(
     [system_message_prompt, human_message_prompt]
 )
 
-# load the document and split it into chunks
-loader = TextLoader("./docs/faq.txt")
-documents = loader.load()
-
-# split it into chunks
-text_splitter = CharacterTextSplitter(chunk_size=100, chunk_overlap=0)
-docs = text_splitter.split_documents(documents)
-
-print(docs)
+model = ChatOpenAI()
 
 
-def main():
-    user_input = "Do you ship to Europe?"
-
-    # LCEL makes it easy to build complex chains from basic components, and supports out of the box functionality such as streaming, parallelism, and logging.
-    chain = chat_prompt | model | str_parser
-    # message = chain.invoke({"question": user_input})
-    # print(message)
+def format_docs(docs):
+    return "\n\n".join([d.page_content for d in docs])
 
 
-if __name__ == "__main__":
-    main()
+def load_documents():
+    """Load a file from path, split it into chunks, embed each chunk and load it into the vector store."""
+    raw_documents = TextLoader("./docs/faq_abc.txt").load()
+    text_splitter = CharacterTextSplitter(chunk_size=100, chunk_overlap=0)
+    return text_splitter.split_documents(raw_documents)
+
+
+def load_embeddings(documents, user_query):
+    """Create a vector store from a set of documents."""
+    db = Chroma.from_documents(documents, OpenAIEmbeddings())
+    docs = db.similarity_search(user_query)
+    print(docs)
+    return db.as_retriever()
+
+
+def generate_response(retriever, query):
+    # Create a prompt template using a template from the config module and input variables
+    # representing the context and question.
+    # create the prompt
+
+    chain = (
+        {"context": retriever, "question": RunnablePassthrough()}
+        | chat_prompt_template
+        | model
+        | StrOutputParser()
+    )
+    return chain.invoke(query)
+
+
+def query(query):
+    documents = load_documents()
+    retriever = load_embeddings(documents, query)
+    response = generate_response(retriever, query)
+    return response
+
